9a5599d4f0a0f59ae8d9f53659bb59241d411648
Hilmy Pratama <94878524+mxzyy@users.noreply.github.com>
2024-08-16 15:15:37 +0700

    Create tailwind.config.js

---
+++ landing-page/app/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "*.{html,js}", // Path ke file HTML dan JS Anda
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["Montserrat", "sans-serif"], 
+      },
+    },
+  },
+  plugins: [],
+};
