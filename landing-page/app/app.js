a7871592719136009119a6d1fe2eef1bac418ec3
Hilmy Pratama <94878524+mxzyy@users.noreply.github.com>
2024-08-19 16:01:11 +0700

    Update app.js

--- landing-page/app/app.js
+++ landing-page/app/app.js
@@ -4,9 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -17,11 +15,17 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/services', (req, res) => {
+  console.log("get services");
   res.sendFile(path.join(__dirname, 'public', 'services.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+  console.log("get contact");
+  res.sendFile(path.join(__dirname,'public', 'contact.html'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 const port = 3000;
